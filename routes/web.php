5ef908ab571c7d77cde9134954e2a7b3a4a05ee7
Imon <contact@imonweb.co.uk>
2025-07-07 23:59:52 +0100

    nav bar

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+// use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
+    // return Inertia::render('welcome');
+    return view('index');
+});
+
+Route::get('/hello', function () {
+    return view('index', [
+        'name' => 'Imon',
+    ]);
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -15,3 +22,13 @@ Route::middleware(['auth', 'verified'])->group(function () {
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
+
+<table><tbody>
+<tr>
+    <th style="text-align: center; font-size:22px;">Luxury Suite:</th><th style="text-align: center;"></th>
+</tr>
+
+<td style="text-align: center; font-size:18px; font-weight: bold">1 Cat</td><<td style="text-align: center; font-size:18px; font-weight: bold">£17 per day (inc VAT)
+</td> 
+
+</tr><tr><td style="text-align: center; font-size:18px; font-weight: bold">2 Cats</td><td style="text-align: center; font-size:18px; font-weight: bold">£24 per day (inc VAT)</td></tr></tbody></table>
